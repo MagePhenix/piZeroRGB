c84cf6ce371229fb764f60eb8574788e755ea77e
MagePhenix <ajzook3@gmail.com>
2022-06-08 12:47:53 -0400

    stuff

--- cycleTest.py
+++ cycleTest.py
@@ -1,12 +1,26 @@
-from time import sleep
+from time import sleep,time
 from colorCycle import colorCycle
 from neopixel import NeoPixel
 import board
 
 
-rgbCycle = colorCycle(["red", "green", "blue"], .5, 3, 15, brightness=.4)
+rgbCycle = colorCycle(["red", "green", "blue"], .1, 3, 15, brightness=.4)
 
 pixels = NeoPixel(board.D18, 118, auto_write=False, bpp=4)
+
+endTime = 10 + time()
+
+while time() < endTime:
+
+    step = rgbCycle.getStep()
+
+    for i in range(len(pixels)):
+
+        pixels[i] = step[i % len(step)]
+
+    pixels.show()
+
+rgbCycle.switchDirection()
 
 while True:
 
