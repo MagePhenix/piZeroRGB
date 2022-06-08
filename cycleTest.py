5dbc4f16330c366dae809ceb39d88458ef5fd868
MagePhenix <ajzook3@gmail.com>
2022-06-08 11:15:37 -0400

    changes

--- cycleTest.py
+++ cycleTest.py
@@ -1,11 +1,13 @@
+from time import sleep
 from colorCycle import colorCycle
 from neopixel import NeoPixel
 import board
 
 
-rgbCycle = colorCycle(["red", "green", "blue"], 2, 5, 3, brightness=.4)
+rgbCycle = colorCycle(["red", "blue"], 1, 3, 0, brightness=.4)
 
 pixels = NeoPixel(board.D18, 59, auto_write=False, bpp=4)
+print(len(pixels))
 
 while True:
 
