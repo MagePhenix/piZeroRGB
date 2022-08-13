2d5ec2085c3a13dbc94089fa6f4ef9cbcc0746a6
MagePhenix <ajzook3@gmail.com>
2022-08-13 10:03:01 -0400

    bug fixes

--- serialTest.py
+++ serialTest.py
@@ -1,3 +1,4 @@
+from ast import Str
 import re
 from sys import byteorder
 import serial
@@ -8,14 +9,14 @@ class serialMonitor:
 
     def __init__(self) -> None:
         self.lastChecked = time.time_ns() - (1000000 * 100)
-        self.ser = serial.Serial('/dev/serial0', 115200, timeout=5)
+        self.ser = serial.Serial('/dev/serial0', 115200, timeout=1)
         self.data = ""
 
-    def getValues(self):
+    def getValues(self, input = '0'):
         if ((time.time_ns() - self.lastChecked) / 1000000) < 100:
             return self.data
 
-        self.ser.write('0'.encode('utf-8'))
+        self.ser.write(input.encode('utf-8'))
 
         input = self.ser.read_until()
         inputArr = input.decode("utf-8").split(',')
@@ -26,6 +27,4 @@ mon = serialMonitor()
 
 while True:
 
-    input("Paused")
-
-    print(mon.getValues())
+    print(mon.getValues(input("Send: ")))
